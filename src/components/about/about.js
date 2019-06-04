7fc2940721716c695c1f2983691f77b6ee3035da
Aditya <aditya510@gmail.com>
2019-06-04 23:18:53 +0530

    First Pull Request for Basic ContactUs and AboutUs pages

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './about.css'
 
 const about = (props) => {
     return (
-        <h1>About</h1>
+
+        <div>
+          <div className='topbox'>
+            <h1 className='whyctf'> Why CTF? </h1>
+            <p className='DottedBox_content'> CTF gives you a platform to learn and show your talent as hackers </p>
+          </div>
+          <div className='subtext'>
+          <h3> About </h3>
+
+          <p> CTF stands for Capture The Flag. A flag is associated with every challenge. </p>
+           
+            <p> Once you have solved a particular challenge you would get the hidden flag which you need to submit back to us for getting points. </p>
+          </div>
+        </div>
+
+
     )
 }
 
