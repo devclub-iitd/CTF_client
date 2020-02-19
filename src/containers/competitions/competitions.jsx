f0d00f5f9de9f71e42029172c7ab365d7d1624e1
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2020-02-19 17:40:08 +0530

    Updated Competitions UI

--- src/containers/competitions/competitions.jsx
+++ src/containers/competitions/competitions.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {
-  Typography, CssBaseline, Paper, Grid, Container, Box,
+  Typography, Paper, Grid, Container, Box, Fab
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import NavigationIcon from '@material-ui/icons/Navigation';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import classes from './competitions.module.css';
@@ -20,35 +21,23 @@ class competitions extends Component {
     let compList = <Spinner />;
     if (competitionsList) {
       compList = Object.values(competitionsList).map(el => (
-        <div className={classes.list} key={el.id}>
-          <Paper>
-            <Link to={`/competition/C${el.id}`}>
-              <div
-                className={classes.title}
-                role="presentation"
-              >
-                <Typography variant="h3" component="h3" align="center">
+        <div key={el.id}>
+          <Paper className={classes.competitionCard} style={{ backgroundColor: 'rgba(240,240,240,0.8)' }}>
+            <Link to={`/competition/C${el.id}`} style={{ textDecoration: 'none' }}>
+              <div className={classes.competitionCardTitle}>
                   {el.name}
-                </Typography>
               </div>
             </Link>
-            <Grid container spacing={3}>
-              <Grid item xs={10}>
-                <div className={classes.padding}>
-                  <Typography variant="h5" component="h3">
-                    {el.details}
-                  </Typography>
-                </div>
-              </Grid>
-              <Grid item xs={2}>
-                  Registered
-                <br />
-                <br />
+            <div className={classes.miniLineCol} />
+            <div className={classes.competitionCardDetailCont}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <div className={classes.thinLine} />
+              <div className={classes.competitionCardExtras}>
+                Registered -
+                {' '}
                 {el.userReg}
-              </Grid>
-
-            </Grid>
-
+              </div>
+            </div>
           </Paper>
         </div>
       ));
@@ -56,12 +45,20 @@ class competitions extends Component {
     return (
       <Box p={5}>
         <Container>
-          <div>
-            <br />
-            <CssBaseline />
-            <Typography variant="h1" align="center">Competitions</Typography>
-            {compList}
-            <Link to="/competitions/add"><Typography variant="h4" align="center">Add a new Competition</Typography></Link>
+          <div className={classes.mainCont}>
+            <div className={classes.pageTitle}>Competitions</div>
+            <div className={classes.miniLine} />
+            <div className={classes.competitionListCont}>
+              {compList}
+            </div>
+            <div className={classes.btnCont}>
+              <Link to="/competitions/add">
+                <Fab variant="extended" color="primary" aria-label="Add">
+                  <NavigationIcon className={classes.extendedIcon} />
+                  Add Competition
+                </Fab>
+              </Link>
+            </div>
           </div>
         </Container>
       </Box>
@@ -75,6 +72,7 @@ competitions.propTypes = {
   onInitCompetitions: PropTypes.node.isRequired,
   competitionsList: PropTypes.node.isRequired,
 };
+// {el.details}
 
 const mapStateToProps = state => ({
   competitionsList: state.competitions,
